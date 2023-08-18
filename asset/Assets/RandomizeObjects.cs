701b0c91c3691628a9238ccc825f54ab26ca6bb5
Enesozd1 <94735890+Enesozd1@users.noreply.github.com>
2023-08-18 14:48:13 +0600

    .

--- asset/Assets/RandomizeObjects.cs
+++ asset/Assets/RandomizeObjects.cs
@@ -5,11 +5,15 @@ using UnityEngine.AI;
 
 public class RandomizeObjects : MonoBehaviour
 {
-    public Transform Obj1;
+
+    //public Transform Obj1;
     public Transform Obj2;
     public Transform Obj3;
     public Transform Obj4;
     public Transform[] spawnpoints;
+
+    public GameObject notedup1;
+    public GameObject notedup2;
 
     int indexNumber;
     int indexNumber2;
@@ -21,16 +25,23 @@ public class RandomizeObjects : MonoBehaviour
     void Start()
     {
         
-            indexNumber = Random.Range(3, 4);
-            indexNumber2 = 8;
-            indexNumber3 = Random.Range(8, 11);
-            indexNumber4 = Random.Range(12, 12);
+            indexNumber = Random.Range(0, 1);
+            indexNumber2 = Random.Range(2, 6); 
+            indexNumber3 = Random.Range(7, 11);
+            indexNumber4 = Random.Range(12, 16);
 
-        
-            
-                
-                Obj1.position = spawnpoints[indexNumber].position;
-                Obj2.position = spawnpoints[indexNumber2].position;
+            if(indexNumber == 0)
+        {
+            notedup1.SetActive(true);
+        }
+        else if (indexNumber == 1)
+        {
+            notedup2.SetActive(true);
+        }
+
+
+
+        Obj2.position = spawnpoints[indexNumber2].position;
                 Obj3.position = spawnpoints[indexNumber3].position;
                 Obj4.position = spawnpoints[indexNumber4].position;
 
@@ -44,6 +55,6 @@ public class RandomizeObjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
