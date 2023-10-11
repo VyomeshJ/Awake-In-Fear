2758d706ad70e6971ec426571ec585c3133016a9
9te <vyomesh.jamwal@gmail.com>
2023-10-11 16:25:12 +0600

    .

--- asset/Assets/you_died_script.cs
+++ asset/Assets/you_died_script.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class you_died_script : MonoBehaviour
 {
     public GameObject GoToMainMenuButton;
-    public GameObject RestartGameButton;
+    public GameObject transition_obj;
+
+    void Start()
+    {
+        File.Delete(Application.dataPath + "/save" + PlayerPrefs.GetInt("SaveNum").ToString() + ".txt");
+    }
     public void end_ani()
     {
         GoToMainMenuButton.SetActive(true);
-        RestartGameButton.SetActive(true);
+    }
+
+    public void go_to_main_menu()
+    {
+        transition_obj.SetActive(true);
+        transition_obj.GetComponent<Animator>().Play("close");
+        StartCoroutine(wait_to_main_menu());
+    }
+    IEnumerator wait_to_main_menu()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("MainMenu");
     }
 }
