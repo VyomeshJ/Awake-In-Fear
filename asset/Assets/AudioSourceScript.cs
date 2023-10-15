ecaee67eca59911d57ccbe30add7d04f3f1d91d4
9te <vyomesh.jamwal@gmail.com>
2023-10-16 00:23:25 +0600

    .

---
+++ asset/Assets/AudioSourceScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioSourceScript : MonoBehaviour
+{
+    public AudioClip start_clip;
+    public AudioClip loop_clip;
+    public AudioClip game_clip;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void FixedUpdate()
+    {
+        if(SceneManager.GetActiveScene().name == "MainMenu" || SceneManager.GetActiveScene().name == "SaveMenu" || SceneManager.GetActiveScene().name == "TutorialScene")
+        {
+            if(gameObject.GetComponent<AudioSource>().clip.name != "loop")
+            {
+                gameObject.GetComponent<AudioSource>().clip = loop_clip;
+                gameObject.GetComponent<AudioSource>().loop = true;
+                gameObject.GetComponent<AudioSource>().Play();
+            }
+         
+        }
+        else if(SceneManager.GetActiveScene().name == "Scene_A")
+        {
+            if (gameObject.GetComponent<AudioSource>().clip.name != "game")
+            {
+                gameObject.GetComponent<AudioSource>().clip = game_clip;
+                gameObject.GetComponent<AudioSource>().loop = true;
+                gameObject.GetComponent<AudioSource>().Play();
+            }
+                
+        }
+        else
+        {
+            gameObject.GetComponent<AudioSource>().Stop();
+        }
+    }
+
+}
